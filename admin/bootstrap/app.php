2be163ee2a9df1d2cc4121a4e25d3f9ccc3ea161
Masukul Miguel <masukulmiguel@gmail.com>
2026-08-04 16:42:01 +0100

    Trust Render proxy for HTTPS

--- admin/bootstrap/app.php
+++ admin/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(SecurityHeaders::class);
+        $middleware->trustProxies(at: '*');
 
         $middleware->validateCsrfTokens(except: [
             '/api/track',
