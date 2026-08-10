1a34e5f795f2592e36a57c7ec304759fa5db27fa
Masukul Miguel <masukulmiguel@gmail.com>
2026-08-10 11:19:41 +0100

    fix: use firstOrCreate instead of updateOrCreate to preserve password + remove emergency route

--- admin/database/seeders/AdminSeeder.php
+++ admin/database/seeders/AdminSeeder.php
@@ -10,7 +10,7 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
+        User::firstOrCreate(
             ['email' => 'admin@tshoot-angola.com'],
             [
                 'name' => 'Admin TSHOOT',
